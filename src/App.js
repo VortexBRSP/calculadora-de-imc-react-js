874eb2bf107a39aec75cbb377565f1b161872a40
Eduardo Lima <edulimacosta223@gmail.com>
2022-06-20 18:41:13 -0300

    Adicionado funções aos botões

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
+import { useCallback, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [peso, setPeso] = useState("");
+  const [altura, setAltura] = useState("");
+  function handleClick() {
+    if (!peso == "" || !altura == "") {
+      alert("Altura: " + altura + " Peso: " + peso);
+    } else {
+      alert("Preencha os campos corretamente!");
+    }
+  }
   return (
     <div className="App">
       <div className="container-calculadora">
-        <div className="input-container"></div>
+        <h1 className="title">CALCULADORA DE IMC</h1>
+        <div className="input-container">
+          <input
+            className="inputs"
+            type="text"
+            value={peso}
+            onChange={(e) => setPeso(e.target.value)}
+            placeholder="Digite seu peso em KG Ex: 65"
+          ></input>
+          <input
+            type="text"
+            value={altura}
+            onChange={(e) => setAltura(e.target.value)}
+            className="inputs"
+            placeholder="Digite sua altura em CM Ex: 170"
+          ></input>
+          <button className="calcularbtn" onClick={handleClick}>
+            Calcular
+          </button>
+        </div>
       </div>
     </div>
   );
