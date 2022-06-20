79d58874b034c4dd5027c4a7d8ec1651ff6fa5da
Eduardo Lima <edulimacosta223@gmail.com>
2022-06-20 18:23:15 -0300

    Adicionado esqueleto incial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container-calculadora">
+        <div className="input-container"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
